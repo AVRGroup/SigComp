eba8b30d532ada32b0c4071bbecd532fa93211f4
Matheus Marques <matheusocmarques@gmail.com>
2018-10-19 19:41:01 -0300

    Atualiza API Integra

--- app/src/Library/Integra/WSLogin.php
+++ app/src/Library/Integra/WSLogin.php
@@ -9,31 +9,31 @@ class WSLogin extends \SoapClient
      * @var array $classmap The defined classes
      */
     private static $classmap = array(
+        'getUserInformation' => 'App\\Library\\Integra\\getUserInformation',
+        'getUserInformationResponse' => 'App\\Library\\Integra\\getUserInformationResponse',
+        'wsUserInfoResponse' => 'App\\Library\\Integra\\wsUserInfoResponse',
+        'profileResponse' => 'App\\Library\\Integra\\profileResponse',
+        'profile' => 'App\\Library\\Integra\\profile',
+        'logout' => 'App\\Library\\Integra\\logout',
+        'logoutResponse' => 'App\\Library\\Integra\\logoutResponse',
+        'isValidProfile' => 'App\\Library\\Integra\\isValidProfile',
+        'isValidProfileResponse' => 'App\\Library\\Integra\\isValidProfileResponse',
         'getPermissions' => 'App\\Library\\Integra\\getPermissions',
         'getPermissionsResponse' => 'App\\Library\\Integra\\getPermissionsResponse',
         'wsPermissionResponse' => 'App\\Library\\Integra\\wsPermissionResponse',
         'loginByCurso' => 'App\\Library\\Integra\\loginByCurso',
         'loginByCursoResponse' => 'App\\Library\\Integra\\loginByCursoResponse',
         'wsLoginResponse' => 'App\\Library\\Integra\\wsLoginResponse',
-        'login' => 'App\\Library\\Integra\\login',
-        'loginResponse' => 'App\\Library\\Integra\\loginResponse',
-        'loginByMatricula' => 'App\\Library\\Integra\\loginByMatricula',
-        'loginByMatriculaResponse' => 'App\\Library\\Integra\\loginByMatriculaResponse',
-        'isValidProfile' => 'App\\Library\\Integra\\isValidProfile',
-        'isValidProfileResponse' => 'App\\Library\\Integra\\isValidProfileResponse',
         'getTokenExpiration' => 'App\\Library\\Integra\\getTokenExpiration',
         'getTokenExpirationResponse' => 'App\\Library\\Integra\\getTokenExpirationResponse',
+        'login' => 'App\\Library\\Integra\\login',
+        'loginResponse' => 'App\\Library\\Integra\\loginResponse',
         'isValidToken' => 'App\\Library\\Integra\\isValidToken',
         'isValidTokenResponse' => 'App\\Library\\Integra\\isValidTokenResponse',
-        'logout' => 'App\\Library\\Integra\\logout',
-        'logoutResponse' => 'App\\Library\\Integra\\logoutResponse',
+        'loginByMatricula' => 'App\\Library\\Integra\\loginByMatricula',
+        'loginByMatriculaResponse' => 'App\\Library\\Integra\\loginByMatriculaResponse',
         'updateUserGroup' => 'App\\Library\\Integra\\updateUserGroup',
         'updateUserGroupResponse' => 'App\\Library\\Integra\\updateUserGroupResponse',
-        'getUserInformation' => 'App\\Library\\Integra\\getUserInformation',
-        'getUserInformationResponse' => 'App\\Library\\Integra\\getUserInformationResponse',
-        'wsUserInfoResponse' => 'App\\Library\\Integra\\wsUserInfoResponse',
-        'profileResponse' => 'App\\Library\\Integra\\profileResponse',
-        'profile' => 'App\\Library\\Integra\\profile',
         'IntegraSoapServiceException' => 'App\\Library\\Integra\\IntegraSoapServiceException',
     );
 
@@ -52,9 +52,36 @@ class WSLogin extends \SoapClient
             'features' => 1,
         ), $options);
         if (!$wsdl) {
-            $wsdl = 'http://integra.ufjf.br/integra/services/soap/login/wslogin?wsdl';
+            $wsdl = 'https://login.integra.nrc.ice.ufjf.br/integra/services/soap/login/wslogin?wsdl';
         }
         parent::__construct($wsdl, $options);
+    }
+
+    /**
+     * @param getUserInformation $parameters
+     * @return getUserInformationResponse
+     */
+    public function getUserInformation(getUserInformation $parameters)
+    {
+        return $this->__soapCall('getUserInformation', array($parameters));
+    }
+
+    /**
+     * @param logout $parameters
+     * @return logoutResponse
+     */
+    public function logout(logout $parameters)
+    {
+        return $this->__soapCall('logout', array($parameters));
+    }
+
+    /**
+     * @param isValidProfile $parameters
+     * @return isValidProfileResponse
+     */
+    public function isValidProfile(isValidProfile $parameters)
+    {
+        return $this->__soapCall('isValidProfile', array($parameters));
     }
 
     /**
@@ -76,39 +103,21 @@ class WSLogin extends \SoapClient
     }
 
     /**
-     * @param login $parameters
-     * @return loginResponse
-     */
-    public function login(login $parameters)
-    {
-        return $this->__soapCall('login', array($parameters));
-    }
-
-    /**
-     * @param isValidProfile $parameters
-     * @return isValidProfileResponse
-     */
-    public function isValidProfile(isValidProfile $parameters)
-    {
-        return $this->__soapCall('isValidProfile', array($parameters));
-    }
-
-    /**
-     * @param loginByMatricula $parameters
-     * @return loginByMatriculaResponse
-     */
-    public function loginByMatricula(loginByMatricula $parameters)
-    {
-        return $this->__soapCall('loginByMatricula', array($parameters));
-    }
-
-    /**
      * @param getTokenExpiration $parameters
      * @return getTokenExpirationResponse
      */
     public function getTokenExpiration(getTokenExpiration $parameters)
     {
         return $this->__soapCall('getTokenExpiration', array($parameters));
+    }
+
+    /**
+     * @param login $parameters
+     * @return loginResponse
+     */
+    public function login(login $parameters)
+    {
+        return $this->__soapCall('login', array($parameters));
     }
 
     /**
@@ -121,12 +130,12 @@ class WSLogin extends \SoapClient
     }
 
     /**
-     * @param logout $parameters
-     * @return logoutResponse
+     * @param loginByMatricula $parameters
+     * @return loginByMatriculaResponse
      */
-    public function logout(logout $parameters)
+    public function loginByMatricula(loginByMatricula $parameters)
     {
-        return $this->__soapCall('logout', array($parameters));
+        return $this->__soapCall('loginByMatricula', array($parameters));
     }
 
     /**
@@ -136,15 +145,6 @@ class WSLogin extends \SoapClient
     public function updateUserGroup(updateUserGroup $parameters)
     {
         return $this->__soapCall('updateUserGroup', array($parameters));
-    }
-
-    /**
-     * @param getUserInformation $parameters
-     * @return getUserInformationResponse
-     */
-    public function getUserInformation(getUserInformation $parameters)
-    {
-        return $this->__soapCall('getUserInformation', array($parameters));
     }
 
 }
