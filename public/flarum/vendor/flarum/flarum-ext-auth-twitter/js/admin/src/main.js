20afdf880316fcbd13d7c467bd9f95d27deb42a8
lidianetpereira <lidianetpereira26@gmail.com>
2018-11-06 21:51:35 -0300

    Remoção do flarum local

--- public/flarum/vendor/flarum/flarum-ext-auth-twitter/js/admin/src/main.js
+++
@@ -1,7 +0,0 @@
-import app from 'flarum/app';
-
-import TwitterSettingsModal from 'flarum/auth/twitter/components/TwitterSettingsModal';
-
-app.initializers.add('flarum-auth-twitter', () => {
-  app.extensionSettings['flarum-auth-twitter'] = () => app.modal.show(new TwitterSettingsModal());
-});
