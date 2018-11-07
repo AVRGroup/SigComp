9ad493fdf99d04506ebeeed7987df696db3ad130
lidianetpereira <lidianetpereira26@gmail.com>
2018-11-06 21:03:50 -0300

    Atualiza teste do Flarum

---
+++ app/src/Library/FlarumHelper.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Library;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Cookie\CookieJar;
+use Slim\Http\Cookies;
+
+class FlarumHelper
+{
+    /**
+     *
+     */
+    static public function doFlarumLogin() {
+        $client = new Client([
+            // Base URI is used with relative requests
+            'base_uri' => 'http://200.131.219.56',
+            // You can set any number of default request options.
+            'timeout'  => 2.0,
+        ]);
+
+        $cookieJar = new CookieJar();
+        $client->request('POST', '/flarum', [
+            'cookies' => $cookieJar,
+            'form_params' => [
+                'identification' => 'projeto',
+                'password' => 'prj#game'
+            ]
+        ]);
+
+        $cookies = new Cookies();
+        $cookies->setDefaults(['path' => '/flarum']);
+
+        foreach ($cookieJar->toArray() as $cookie) {
+            $cookies->set($cookie['Name'], $cookie['Value']);
+        }
+
+        return $cookies;
+    }
+}
