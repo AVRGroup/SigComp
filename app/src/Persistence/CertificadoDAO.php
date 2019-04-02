2f9b6c000667cd7e844a7d20271c1a6d132b9bdf
Thales Castro <thalescastro.m@gmail.com>
2019-04-02 18:17:57 -0300

    agora ele so esta imprimindo no PDF os certificiados validados

--- app/src/Persistence/CertificadoDAO.php
+++ app/src/Persistence/CertificadoDAO.php
@@ -20,7 +20,7 @@ class CertificadoDAO extends BaseDAO
     public function getAllByUsuario(Usuario $usuario)
     {
         try {
-            $query = $this->em->createQuery("SELECT c, u FROM App\Model\Certificado AS c JOIN c.usuario AS u WHERE c.usuario = :usuario ORDER BY c.data_inicio ASC");
+            $query = $this->em->createQuery("SELECT c, u FROM App\Model\Certificado AS c JOIN c.usuario AS u WHERE c.usuario = :usuario AND c.valido = 1 ORDER BY c.data_inicio ASC");
             $query->setParameter('usuario', $usuario);
             $certificados = $query->getResult();
         } catch (\Exception $e) {
